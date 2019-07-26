5ca77d380c39137824c1c1cbf0bb2aa3728dd54f
pawnesh <pawnesh.k.rai@gmail.com>
2019-07-26 17:31:52 +0530

    init

---
+++ build/precache-manifest.d2b3f696b1a574221838d4496e7b1d09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17dcabfd9ce69a22bb22311443c69260",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfde4ad29f79826b3b9",
+    "url": "/static/css/main.fc3b5979.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "6bfde4ad29f79826b3b9",
+    "url": "/static/js/main.65c05ea5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
