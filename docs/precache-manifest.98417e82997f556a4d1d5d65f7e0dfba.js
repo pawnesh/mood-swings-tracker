dd7906bfd151e6f26ba94f9dc99dc463419e1b65
pawnesh <pawnesh.k.rai@gmail.com>
2019-08-05 17:20:26 +0530

    version 0.4 build

---
+++ docs/precache-manifest.98417e82997f556a4d1d5d65f7e0dfba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f5cf7b6a682c82152264d763dca7000",
+    "url": "/mood-swings-tracker/index.html"
+  },
+  {
+    "revision": "f1c4fe1aa81247033a1a",
+    "url": "/mood-swings-tracker/static/css/main.8d8bf241.chunk.css"
+  },
+  {
+    "revision": "0367de345e5386181b9f",
+    "url": "/mood-swings-tracker/static/js/2.24440903.chunk.js"
+  },
+  {
+    "revision": "f1c4fe1aa81247033a1a",
+    "url": "/mood-swings-tracker/static/js/main.60f6f58f.chunk.js"
+  },
+  {
+    "revision": "b3af2ca9233e42c65a51",
+    "url": "/mood-swings-tracker/static/js/runtime~main.fcbc3a10.js"
+  }
+]);
